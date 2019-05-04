203d1c501c622f64263117fbb353ded0ba6fc740
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2019-05-04 03:16:06 -0400

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,25 +1,42 @@
-public class Process {
+import java.util.*;
 
+public class Process {
+    // Datafields
     int processNumber;
     int processSize;
     int nextWord;
     int numTimesReferenced;
+    int LRUpage;
+    int LRUframe;
+    int nextFrame;
+    List<Integer> pageTable; // Holds a frametable for each page
+    List<List<Integer>> timesLoaded; // Records times loaded
+    List<List<Integer>> timesEvicted; // Records times evicted
+
+    static int globalFrame;
 
     // Probabilities
     double probA;
     double probB;
     double probC;
 
-    public Process() {
+    // Statistics
+    int numPageFaults; // Number of times the process was faulted.
+    int numEvictions; // Number of times the process was evicted.
+
+    public Process(int numPages, int numFrames) {
         this.numTimesReferenced = 0;
         this.nextWord = -1;
-    }
+        this.pageTable = new ArrayList<>();
+        this.timesLoaded = new ArrayList<>();
+        this.timesEvicted = new ArrayList<>();
 
-    public Process(int processSize, int processNumber) {
-        this.processSize = processSize;
-        this.processNumber = processNumber;
-        this.nextWord = (111 * processNumber) % processSize;
+        for (int i = 0; i < numPages; i++) {
+            this.pageTable.add(-1);
+            this.timesLoaded.add(new ArrayList<>());
+            this.timesEvicted.add(new ArrayList<>());
+        }
+    
     }
-
 
 }
